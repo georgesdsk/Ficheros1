0f3983d85dced0f1f88e95b6cbb1bd47baa6f6ac
George <heorhi.sadouski@gmail.com>
2021-05-03 14:37:12 +0200

    Initial commit

---
+++ src/Clases/Main.java
@@ -0,0 +1,14 @@
+package Clases;
+
+import java.io.File;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        File file = new File("./");
+        imprimirContenido(File file);
+
+    }
+}
